096966ef92839cf5efa8a11173cd2f93cbd08cb4
Joonyoung Yi <joonyoung@bablabs.com>
2018-03-09 11:25:03 +0900

    First Commit After Class

---
+++ app/src/main/java/kaist/cs492/dontinsult/Test.java
@@ -0,0 +1,11 @@
+package kaist.cs492.dontinsult;
+
+/**
+ * Created by Woohyeok on 2015-06-17.
+ */
+public class Test {
+    public static void main(String args[]) throws Exception {
+
+
+    }
+}
---
+++ app/src/main/java/kaist/cs492/dontinsult/audio/WavFileException.java
@@ -0,0 +1,19 @@
+package kaist.cs492.dontinsult.audio;
+
+public class WavFileException extends Exception {
+    public WavFileException() {
+        super();
+    }
+
+    public WavFileException(String message) {
+        super(message);
+    }
+
+    public WavFileException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public WavFileException(Throwable cause) {
+        super(cause);
+    }
+}
